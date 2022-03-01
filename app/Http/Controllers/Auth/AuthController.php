937c97ccc2a5d6dacb757173ad05eaf73c49f637
yuman211 <yukinuma0211@gmail.com>
2022-03-01 21:03:22 +0900

    create account lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
 
 class AuthController extends Controller
 {
@@ -26,14 +28,45 @@ class AuthController extends Controller
      */
     public function login(LoginFormRequest $request)
     {
-
         $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+        //アカウントがロックされていたら弾く
+        $user = User::where('email','=',$credentials['email'])->first();
 
-            return redirect()->route('home')->with('success', 'ログイン成功しました!');
+        if(!is_null($user)){
+            if($user->locked_flg === 1){
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています。',
+
+                ]);
+            }
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                //成功したらエラーカウントを０にする
+                if($user->error_count > 0){
+                    $user->error_count = 0;
+                    $user->save();
+                }
+
+                return redirect()->route('home')->with('success', 'ログイン成功しました!');
+            }
+
+        //ログイン失敗したらエラーカウントを１増やす
+            $user->error_count += 1;
+
+        //エラーカウントが６以上の場合はアカウントをロックする
+            if($user->error_count >5){
+                $user->locked_flg =1;
+                $user->save();
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされました。解除したい場合は運営者に連絡してください。',
+
+                ]);
+            }
+            $user->save();
+
         }
+
 
         return back()->withErrors([
             'danger' => 'メールアドレスかパスワードが間違っています。',
